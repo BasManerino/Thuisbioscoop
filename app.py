5ebd91e751a3a23f04fa00a0fca7ce47fd0504cb
skottdekoster <skottdekoster@hotmail.nl>
2018-10-31 16:45:10 +0100

    Uitlogsysteem + documentatie
    uitlogsysteem werkt en documentatie in code gezet

--- app.py
+++ app.py
@@ -16,11 +16,13 @@ import uuid
 
 root = tk.Tk ()
 
+#stel in dat menu standaard 0 is, dit betekend dat bezoekers niet op aanbiederpagina's kunnen komen
 global menu
 menu = 0
 
 global aanbieder
 
+#Maak een frame aan met de kleur paars
 class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
@@ -28,6 +30,7 @@ class Page(tk.Frame):
         self.lift()
         self.configure(bg="purple")
 
+#Maak de homepagina aan, met 2 labels en het plaatje
 class Homepage(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
@@ -46,6 +49,7 @@ class Homepage(Page):
        h2.pack (fill=None, expand=True)
        picture.pack (fill=None, expand=True)
 
+#het filmoverzicht voor de bezoeker maken
 class FilmOverzichtBezoeker(Page):
    def __init__(self, film):
        Page.__init__(self, film)
@@ -53,6 +57,7 @@ class FilmOverzichtBezoeker(Page):
        container = tk.Frame (self)
        container.pack (side="top", fill="both", expand=True)
 
+#de cover van de film word opgehaald uit het api bestand en omgezet zodat we hem kunnen gebruiken
        def download_cover(url, naam):
            name = (str (naam) + '.jpg')
            urllib.request.urlretrieve (url, name)
@@ -62,11 +67,14 @@ class FilmOverzichtBezoeker(Page):
        date = time.strftime ("%d-%m-%Y")
        print (date)
 
+#verbinden met mongodb database
        def data():
            print ('Connecting to database...')
            connection = pymongo.MongoClient ('ds159489.mlab.com', 59489)
            db = connection['thuisbioscoop']
            db.authenticate ("test123", "wachtwoord123")
+
+#als er connectie is print je dat en begin je met labels in te laden waar data in komt
            if db.is_mongos:
                print ('Connection successful.')
                mycol = db["films"]
@@ -95,6 +103,7 @@ class FilmOverzichtBezoeker(Page):
 
                row = 1
 
+#variabelen omzetten om door te geven
                def get_film(film, starttijd, eindtijd):
                    global filmtitel
                    filmtitel = film
@@ -104,6 +113,7 @@ class FilmOverzichtBezoeker(Page):
                    eind = eindtijd
                    p1.lift()
 
+# Alle opgehaalde data in de labels zetten
                for film in findStats:
                    titel = tk.Label (frame, text=film['Titel'].encode ("utf-8"), font="Helvetica 18 bold", fg="white",
                                   bg="purple")
@@ -123,6 +133,7 @@ class FilmOverzichtBezoeker(Page):
                    action = partial(get_film,film['Titel'],film["Starttijd"],film["Eindtijd"])
                    button = tk.Button (frame, text='Reserveer', command=action)
 
+#alles in een grid zetten om er een overzicht van te maken
                    cover.grid (row=row, column=1, sticky=tk.NW)
                    titel.grid (row=row, column=2, sticky=tk.NW)
                    genre.grid (row=row, column=3, sticky=tk.NW)
@@ -131,19 +142,12 @@ class FilmOverzichtBezoeker(Page):
                    adres.grid (row=row, column=6, sticky=tk.NW)
                    button.grid (row=row, column=7, sticky=tk.NW)
 
-                   """
-                   st = int (film['starttijd'])
-                   starttijd = (datetime.utcfromtimestamp (st).strftime ('%m-%d %H:%M'))
-
-
-                   et = int (film['eindtijd'])
-                   eindtijd = (datetime.utcfromtimestamp (et).strftime ('%m-%d %H:%M'))
-                   """
                    row += 1
 
        p1 = LoginBezoeker(self)
        p1.place (in_=container, x=0, y=0, relwidth=1, relheight=1)
 
+#scrollbar inladen
        def myfunction(event):
            canvas.configure (scrollregion=canvas.bbox ("all"), width=1500, height=800)
 
@@ -167,11 +171,8 @@ class FilmOverzichtBezoeker(Page):
        frame.bind ("<Configure>", myfunction)
 
        data ()
-       # root.mainloop ()
 
-       # for p in Path (".").glob ("*.jpg"):
-       #     p.unlink ()
-
+#login pagina voor bezoeker aanmaken en ticketpagina genereren
 class LoginBezoeker (Page):
     def __init__(self, *args):
         Page.__init__ (self, *args)
@@ -182,6 +183,7 @@ class LoginBezoeker (Page):
                                   font='Helvetica 16 bold', height=2, bg="purple", fg="white")
         loginheaderlabel.pack ()
 
+#maak random code aan
         def randomword(length):
             letters = string.ascii_lowercase
             return str(''.join (random.choice (letters) for i in range (length)))
@@ -190,11 +192,13 @@ class LoginBezoeker (Page):
 
         def bezoeken(naam, email, film, starttijd, eindtijd, word):
 
-            # Create an image from the QR Code instance
+#maak weer verbinding met mongodb database
             print ('Connecting to database...')
             connection = pymongo.MongoClient ('ds159489.mlab.com', 59489)
             db = connection['thuisbioscoop']
             db.authenticate ("test123", "wachtwoord123")
+
+#als er verbinding is met mongodb haal dan de info die op de pagina te zien moet zijn op
             if db.is_mongos:
                 print ('Connection successful.')
                 mycol2 = db["films"]
@@ -218,18 +222,17 @@ class LoginBezoeker (Page):
                 adres = tk.Label (master=self, text="Adres: " + findStats2["Adres"], background="purple", foreground="white")
                 adres.pack (pady=10, padx=10, )
 
-                # Add data
+#zet de data in de QR-code
                 qr.add_data (data)
                 qr.make (fit=True)
 
-                # Create an image from the QR Code instance
+#maak van de data een qr-code image
                 img = qr.make_image ()
 
-                # Save it somewhere, change the extension as needed:
-                # img.save("image.png")
-                # img.save("image.bmp")
-                # img.save("image.jpeg")
+#en sla deze op
                 img.save ("QR.jpg")
+
+#alle informatie inlezen op de pagina
                 qrframe = tk.Frame (master=self)
                 codelabel = tk.Label (master=self, text="Code: " + data, background="purple", foreground="white")
                 codelabel.pack (pady=10, padx=10, )
@@ -255,28 +258,33 @@ class LoginBezoeker (Page):
         emailEntry = tk.Entry (master=self)
         emailEntry.pack (padx=10, pady=10)
 
+#pak de ingevoerde naam en het emailadres en de andere info
+
         button = tk.Button (master=self, text='OK',
                          command=lambda:[bezoeken(naamEntry.get(), emailEntry.get(), filmtitel, start, eind, woordje), button.destroy()])
         button.pack (pady=10)
 
         scancodetxt = tk.Label (master=self,
-                                text="Zodra u uw gegevens heeft ingevuld en verzonden zal er hier een QR code verschijnen. Scan deze om uw toegangscode te verkrijgen.",
+                                text="Als u uw gegevens heeft ingelogd krijgt u een scherm te zien met een code en een QR-code, dit zijn uw toegangscodes. Maak een screenshot of foto van deze pagina om als bewijs te laten zien voor de film."   ,
                                 background="purple", foreground="white")
         scancodetxt.pack (pady=10, padx=10, fill=None, expand=True)
 
+#Registratiepagina voor aanbieders
 class RegistrerenAanbieder(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__ (self, *args, **kwargs)
         self.configure(bg="purple")
 
+#het wachtwoord dat word ingevoerd hashen om het veilig op te slaan
         def hash_password(password):
-            # uuid is used to generate a random number
+#we gebruiken uuid om een random nummer te maken
             salt = uuid.uuid4 ().hex
             return hashlib.sha256 (salt.encode () + password.encode ()).hexdigest () + ':' + salt
 
         registerheaderlabel = tk.Label (master=self, text='Registreer aanbieders', font='Helvetica 16 bold', height=2, fg="white", bg="purple")
         registerheaderlabel.pack ()
 
+#registreer de aanbieder in de database
         def register(naam, adres, wachtwoord):
             hashed_password = hash_password(wachtwoord)
             print ('Connecting to database...')
@@ -321,6 +329,7 @@ class RegistrerenAanbieder(Page):
                          command=lambda:[register (naamEntry.get (), adresEntry.get (), wachtwoordEntry.get ()),button.destroy()])
         button.pack (pady=10)
 
+#loginpagina van de aanbieder
 class LoginAanbieder(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__ (self, *args, **kwargs)
@@ -344,17 +353,20 @@ class LoginAanbieder(Page):
         button = tk.Button (master=self, text='Login', command=lambda: login(naamEntry.get (), wachtwoordEntry.get ()))
         button.pack (pady=10)
 
+#het filmoverzicht voor de aanbieder
 class FilmOverzichtAanbieder(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
        date = time.strftime ("%d-%m-%Y")
 
+#met de api verbinding maken
        api_url = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey=djg2nqcl4jpkwivqgjdox3klob4zyx3d&dag=' + str (
            date) + '&sorteer=0'
        response = requests.get (api_url)
 
        filmXML = xmltodict.parse (response.text)
 
+#weer de cover ophalen
        def download_cover(url, naam):
            name = (str (naam) + '.jpg')
            urllib.request.urlretrieve (url, name)
@@ -363,7 +375,9 @@ class FilmOverzichtAanbieder(Page):
 
        date = time.strftime ("%d-%m-%Y")
 
+#zorg dat een film aangeboden kan worden en dit in het systeem met alle informatie weggeschreven word
        def bied_aan(titel, cover, genre, starttijd, eindtijd, naam_aanbieder, datum, row):
+#haal de ingelogde aanbieder op en verbind hem met de aangeboden film
            def check(aanbieder):
                connection = pymongo.MongoClient ('ds159489.mlab.com', 59489)
                db = connection['thuisbioscoop']
@@ -398,6 +412,7 @@ class FilmOverzichtAanbieder(Page):
                melding = tk.Label (frame, text='De film is succesvol aangeboden.', height=2, fg="white", bg="purple")
                melding.grid(row=row, column=7, sticky=tk.NW)
 
+#laad alle data van de aangeboden film in
        def data(aanbieder):
            api_url = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey=djg2nqcl4jpkwivqgjdox3klob4zyx3d&dag=' + str (
                date) + '&sorteer=0'
@@ -446,6 +461,7 @@ class FilmOverzichtAanbieder(Page):
 
            mycol = db["films"]
 
+#check of de film al aangeboden word en zo ja disable de button
            def check_aanbieding(mycol, titel, row, action):
                callstats = {"Titel": titel}
                findStats = mycol.find_one (callstats)
@@ -461,6 +477,7 @@ class FilmOverzichtAanbieder(Page):
 
 
 
+#laad alle filminfo in en loop er doorheen
            for film in filmXML['filmsoptv']['film']:
                st = int (film['starttijd'])
                starttijd = (datetime.utcfromtimestamp (st).strftime ('%H:%M'))
@@ -470,7 +487,6 @@ class FilmOverzichtAanbieder(Page):
 
                titel = tk.Label (frame, text=film['titel'].encode ("utf-8"), font="Helvetica 18 bold", fg="white",
                               bg="purple")
-               # canvas = Canvas (frame)
                img = ImageTk.PhotoImage (Image.open (download_cover (film['cover'], titel)))
                cover = tk.Label (frame, image=img)
                cover.image = img
@@ -496,6 +512,7 @@ class FilmOverzichtAanbieder(Page):
 
                row += 1
 
+#zorg weer voor een scrollbar
        def myfunction(event):
            canvas.configure (scrollregion=canvas.bbox ("all"), width=1500, height=800)
        sizex = 800
@@ -518,10 +535,12 @@ class FilmOverzichtAanbieder(Page):
        frame.bind ("<Configure>", myfunction)
        data (aanbieder)
 
+#overizcht voor de aanbieder van zijn/haar bezoekers
 class OverzichtVanBezoekers(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
 
+#laad weer de cover
        def download_cover(url, naam):
            name = (str (naam) + '.jpg')
            urllib.request.urlretrieve (url, name)
@@ -531,6 +550,7 @@ class OverzichtVanBezoekers(Page):
        date = time.strftime ("%d-%m-%Y")
        print (date)
 
+#haal data op van bezoekers die naar een film komen
        def data():
            print ('Connecting to database...')
            connection = pymongo.MongoClient ('ds159489.mlab.com', 59489)
@@ -554,7 +574,7 @@ class OverzichtVanBezoekers(Page):
                code.grid (row=0, column=3, sticky=tk.NW)
 
                row = 1
-
+#laat deze data zien
                for film in findStats:
                    titel = tk.Label (frame, text=film['Film'].encode ("utf-8"), font="Helvetica 18 bold", fg="blue", bg="purple")
                    bezoekernaam = tk.Label (frame, text=film['Naam'].encode ("utf-8"), font="Helvetica 18 bold", fg="red", bg="purple")
@@ -569,6 +589,7 @@ class OverzichtVanBezoekers(Page):
        container = tk.Frame (self)
        container.pack (side="top", fill="both", expand=True)
 
+#zorg weer dat de scrollbar werkt
        def myfunction(event):
            canvas.configure (scrollregion=canvas.bbox ("all"), width=1500, height=800)
 
@@ -593,6 +614,7 @@ class OverzichtVanBezoekers(Page):
 
        data ()
 
+#houd alle pagina's bij elkaar en zorgt dat ze ingeladen kunnen worden
 class MainView(tk.Frame):
     global aanbieder
     aanbieder = None
@@ -602,6 +624,7 @@ class MainView(tk.Frame):
         container = tk.Frame(self)
         container.pack (side="top", fill="both", expand=True)
 
+#laad alle pagina's al vantevoren in
         def set_menu(self):
             p1 = Homepage (self)
             p2 = FilmOverzichtBezoeker (self)
@@ -617,6 +640,7 @@ class MainView(tk.Frame):
             p5.place (in_=container, x=0, y=0, relwidth=1, relheight=1)
             p6.place (in_=container, x=0, y=0, relwidth=1, relheight=1)
 
+#als menu == 0 is betekend dit dat dit een bezoeker is en krijgt deze toegang tot andere schermen
             if (menu == 0):
                 menuBezoeker = tk.Menu (root)
                 home = tk.Menu (menuBezoeker, tearoff=0)
@@ -634,6 +658,7 @@ class MainView(tk.Frame):
 
                 root.config (menu=menuBezoeker)
 
+#elif menu == 1 betekend dat dit een aanbieder is en krijgt dus toegang tot de aanbieder schermen
             elif (menu == 1):
                 menuaanbieder = tk.Menu (root)
                 home = tk.Menu (menuaanbieder, tearoff=0)
@@ -648,16 +673,22 @@ class MainView(tk.Frame):
                 bezoekers.add_command (label="Overzicht van bezoekers", command=p6.lift)
                 menuaanbieder.add_cascade (label="Bezoekers", menu=bezoekers)
 
+                uitloggen = tk.Menu (menuaanbieder, tearoff=0)
+                uitloggen.add_command (label="Uitloggen", command=logout)
+                menuaanbieder.add_cascade (label="Uitloggen", menu=uitloggen)
+
                 root.config (menu=menuaanbieder)
 
             p1.show ()
-
+#zet het gehasde wachtwoord weer terug
         def check_password(hashed_password, user_password):
             password, salt = hashed_password.split (':')
             return password == hashlib.sha256 (salt.encode () + user_password.encode ()).hexdigest ()
         self.configure(bg="purple")
 
         global login
+
+#als iemand inlogt zet menu op 1 zodat de aanbieder bij zijn schermen kan
         def login(naam, wachtwoord):
             global aanbieder
             aanbieder = naam
@@ -672,6 +703,7 @@ class MainView(tk.Frame):
                 callstats = {"Naam": naam}
 
                 findStats = mycol.find_one(callstats)
+#hier word gekeken of het een aanbieder is en word het menu op 1 gezet zo ja
                 if(findStats and check_password (findStats["Wachtwoord"], wachtwoord)):
                     global menu
                     menu = 1
@@ -680,6 +712,17 @@ class MainView(tk.Frame):
 
         set_menu(self)
 
+        global logout
+
+        def logout():
+                    global menu
+                    menu = 0
+                    set_menu(self)
+                    print(menu)
+
+        set_menu(self)
+
+#zorg dat de view werkt
 if __name__ == "__main__":
     main = MainView(root)
     main.pack(side="top", fill="both", expand=True)
